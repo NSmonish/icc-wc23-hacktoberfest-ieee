088d09ef6679270053d9ee1eb7d740ceb6f713b7
nish_hack_06 <83601466+NSmonish@users.noreply.github.com>
2023-10-05 21:49:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,35 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
+#Create a Flask Instance
 app = Flask(__name__)
+
+#Add Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fixtures.db'
+
+#Initialize Database
+db = SQLAlchemy(app)
+
+#Create Model
+class Fixtures(db.Model):
+    time = db.Column(db.String(5))
+    day_month = db.Column(db.String(10))
+    date = db.Column(db.String(10))
+    home_team = db.Column(db.String(200), nullable=False)
+    away_team = db.Column(db.String(200), nullable=False)
+    venue = db.Column(db.String(200))
+    matchNumber = db.Column(db.Integer, primary_key=True)
+    roundNumber = db.Column(db.Integer, nullable=False)
+    
+    def __repr__ (self):
+        return '<matchNumber %r>' % self.venue
+    
+from app import Fixtures
+
+with app.app_context():
+    db.create_all()
+    
 
 @app.route('/')
 def index():
