d1144fb4a8f722949a814b8d9d41a8f608bdd18b
nish_hack_06 <83601466+NSmonish@users.noreply.github.com>
2023-10-06 22:10:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,31 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+#from models import Fixtures
 
 #Create a Flask Instance
 app = Flask(__name__)
-
-#Add Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fixtures.db'
 
 #Initialize Database
 db = SQLAlchemy(app)
-
-#Create Model
+    
 class Fixtures(db.Model):
-    time = db.Column(db.String(5))
-    day_month = db.Column(db.String(10))
-    date = db.Column(db.String(10))
-    home_team = db.Column(db.String(200), nullable=False)
-    away_team = db.Column(db.String(200), nullable=False)
-    venue = db.Column(db.String(200))
-    matchNumber = db.Column(db.Integer, primary_key=True)
-    roundNumber = db.Column(db.Integer, nullable=False)
+    id=db.Column(db.Integer,primary_key=True)
+    time=db.Column(db.String(5))
+    day_month=db.Column(db.String(10))
+    date=db.Column(db.String(100))
+    team_1=db.Column(db.String(200),nullable=False)
+    team_2=db.Column(db.String(200),nullable=False)
+    venue=db.Column(db.String(200))
+    matchNumber=db.Column(db.String(3),nullable=False)
     
     def __repr__ (self):
         return '<matchNumber %r>' % self.venue
     
-from app import Fixtures
-
 with app.app_context():
     db.create_all()
     
-
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -41,6 +36,9 @@ def standings():
 
 @app.route('/fixtures')
 def fixtures():
+    fixtures = Fixtures(time="14:00",day_month="Friday Oct",date="05/10/23",team_1="New Zealand",team_2="England",venue="Narendra Modi Staduium",matchNumber="1")
+    db.session.add(fixtures)
+    db.session.commit()
     return render_template('fixtures.html')
 
 if __name__ == "__main__":
